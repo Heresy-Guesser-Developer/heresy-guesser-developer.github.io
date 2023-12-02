de4a721841292804b11303bc5d6d30ba904b7c43
Fede2617 <92307585+Fede2617@users.noreply.github.com>
2023-12-02 19:50:46 +0100

    version 0.004 (#10)
    * correct file 0.02
    
    * 0.003
    
    Da sistemare logica per hide della frase, controllare che tutto funzioni con i row-container
    
    * aggiunta logica di gioco, da finire match char e colori

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ const tutorialMessage = document.querySelector(".tutorial-message");
 const newGameButton = document.querySelector(".new-game");
 let arrayWords = ["maiale", "bastardo", "lurido", "ebreo", "maledetto", "cane", "boia"];
 let randomWord;
+const rowContainer0 = document.querySelector(".row-container0");
+const rowContainer1 = document.querySelector(".row-container1");
+const rowContainer2 = document.querySelector(".row-container2");
+const rowContainer3 = document.querySelector(".row-container3");
+const rowContainer4 = document.querySelector(".row-container4");
+const rowContainer5 = document.querySelector(".row-container5");
+const rowContainer6 = document.querySelector(".row-container6");
+const submitButton = document.getElementById("submit-id");
+const innerText = document.createElement("div");
+const rows = document.createElement("div");
 
 //Functions //
 
@@ -30,15 +40,9 @@ const gotFunction = function () {
     }
 }
 
-const newGame = function () {
-    const randomIndex = Math.floor((Math.random() * arrayWords.length));
-    randomWord = arrayWords[randomIndex]
-    console.log(randomWord);
-    createGrid(randomWord.length);
-    return randomWord
 
-<<<<<<< HEAD
 const newGame = function () {
+    rowContainer0.innerHTML = ""
     const randomIndex = Math.floor((Math.random() * arrayWords.length));
     randomWord = arrayWords[randomIndex]
     console.log(randomWord);
@@ -49,61 +53,44 @@ const newGame = function () {
 }
 
 const createGrid = function (length) {
-    const gridContainer = document.getElementById("grid-container");
-    const cellContainer = document.querySelector(".cell-container");
-    const rowContainer0 = document.querySelector(".row-container0");
-    const rowContainer1 = document.querySelector(".row-container1");
-    const rowContainer2 = document.querySelector(".row-container2");
-    const rowContainer3 = document.querySelector(".row-container3");
-    const rowContainer4 = document.querySelector(".row-container4");
-    const rowContainer5 = document.querySelector(".row-container5");
-    const rowContainer6 = document.querySelector(".row-container6");
+
 
     for (let col = 0; col < length; col++) {
-        const cell = document.createElement("div");
         const rows = document.createElement("div");
+        const innerText = document.createElement("div");
         rows.classList.add("grid-row");
-        cell.classList.add("grid-cell");
+        innerText.classList.add("hide")
         let slicedWord = randomWord[col]
-        cell.innerHTML = slicedWord;
-        rows.innerHTML = slicedWord;
-        cellContainer.appendChild(cell);
+        innerText.innerHTML = slicedWord;
+        rowContainer0.appendChild(innerText);
         rowContainer0.appendChild(rows);
-        
+
     }
-    rowContainer1.innerHTML = rowContainer0.innerHTML
-    rowContainer2.innerHTML = rowContainer1.innerHTML
-    rowContainer3.innerHTML = rowContainer2.innerHTML
-    rowContainer4.innerHTML = rowContainer3.innerHTML
-    rowContainer5.innerHTML = rowContainer4.innerHTML
-    rowContainer6.innerHTML = rowContainer5.innerHTML
+    rowContainer1.innerHTML = rowContainer0.innerHTML;
+    rowContainer2.innerHTML = rowContainer1.innerHTML;
+    rowContainer3.innerHTML = rowContainer2.innerHTML;
+    rowContainer4.innerHTML = rowContainer3.innerHTML;
+    rowContainer5.innerHTML = rowContainer4.innerHTML;
+    rowContainer6.innerHTML = rowContainer5.innerHTML;
 }
+
+
+const submitButtonFunction = function () {
+    const answer = document.getElementById("input-text").value;
+    console.log(answer);
+    if (answer === randomWord) {
+        document.querySelectorAll(".grid-row").forEach(function (rows) {
+            rows.style.opacity = 0;
+        })
+        document.querySelectorAll(".hide").forEach(function (innerText) {
+            innerText.style.opacity = 1;
+        })
+        console.log("Ho rimosso la classe");
+    }
+}
+
+
 
 gotItButton.addEventListener("click", gotFunction); // debugging
-=======
-
-}
-
-const createGrid = function (wordLength) {
-    const gridContainer = document.getElementById("grid-container");
-    const numColumns = 10;
-    gridContainer.innerHTML = '';
-
-    let charIndex = 0;
-
-
-    const totalColumns = Math.ceil(wordLength / 5) * 5;
-
-
-    for (let col = 0; col < totalColumns && charIndex < wordLength; col++) {
-        const cell = document.createElement("div");
-        cell.classList.add("grid-cell");
-        cell.textContent = randomWord[charIndex];
-        gridContainer.appendChild(cell);
-        charIndex++;
-    }
-}
-
-gotItButton.addEventListener("click", gotFunction);
->>>>>>> main
 newGameButton.addEventListener("click", newGame);
+submitButton.addEventListener("click", submitButtonFunction);
